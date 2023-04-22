440af35e4df826435a088228723350b1e0774cfc
dslnikkiabq <dslnikkiabq@gmail.com>
2023-04-22 13:56:28 -0600

    added more to scripts.js

---
+++ scripts.js
@@ -0,0 +1,10 @@
+// steps to make sure you are taking
+// second commit and everything after
+// git add .
+// git commit -m "your commit message here"
+// git push
+function greeting(name) {
+    return "Hello, " + name;
+}
+
+console.log(greeting("Nikki")); // Hello Nikki 
